8d2f978a58647f2544fd64dea0f3436cd98d63fe
Jquesada10 <jeffreyquesada10@gmail.com>
2025-02-17 23:42:57 -0600

    menu

---
+++ parte_superior.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<HTML lang="Spanish">
+<head>
+    <meta charset="utf-8">
+    <meta name="Proyecto Tercera" content="Proyecto Tercera">
+    <link real="stylesheet" href="./css/main.css"
+    
+</head>
+
+<menu>
+<div class="menu-up">
+<img src="./img/new-rocket.png"alt="" height="55">
+</div>
+
+<div class="menu-down">
+<div class="menu-izquierda">
+    <a href="">News</a>
+    <a href="">Discord Webhook</a>
+    <a href="">Database</a>
+    <a href="">Tools</a>
+    <a href="">Guides</a>
+    <a href="">Community</a>
+    <a href="">Premium</a>
+
+</div>
+<div class="menu-derecha">
+
+</menu>
