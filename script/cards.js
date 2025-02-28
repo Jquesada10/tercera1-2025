9b66c61b7b982f16df5a3e0bb43bfd40105ea132
Jquesada10 <jeffreyquesada10@gmail.com>
2025-02-27 23:35:41 -0600

    cards

--- script/cards.js
+++ script/cards.js
@@ -1,57 +1,54 @@
-console.log('cargando cards')
+console.log('cargando cards');
 
 const dataCards = [
-{
-"title":"Juego del año",
-"url_image":"./img/goty.png",
-"desc":"Este impresionante juego hecho para los mas exigentes , requiere de mucha habilidad ",
-"cta":"Mostrar mas...",
-"link":"https://www.chiclestore.com/product/sekiro-shadows-die-twice-edicion-juego-del-ano-2/"
-},
-{
-    "title":"Juego del año",
-"url_image":"",
-"desc":"Este impresionante juego hecho para los mas exigentes , requiere de mucha habilidad ",
-"cta":"Mostrar mas...",
-"link":"https://www.chiclestore.com/product/sekiro-shadows-die-twice-edicion-juego-del-ano-2/"
-},
-{
-    "title":"Juego del año",
-"url_image":"",
-"desc":"Este impresionante juego hecho para los mas exigentes , requiere de mucha habilidad ",
-"cta":"Mostrar mas...",
-"link":"https://www.chiclestore.com/product/sekiro-shadows-die-twice-edicion-juego-del-ano-2/"
-}
-
+  {
+    "title": "Juego del año",
+    "url_image": "./img/goty.png",
+    "desc": "Este impresionante juego hecho para los más exigentes, requiere de mucha habilidad.",
+    "cta": "Mostrar más...",
+    "link": "https://www.chiclestore.com/product/sekiro-shadows-die-twice-edicion-juego-del-ano-2/"
+  },
+  {
+    "title": "Juego del año",
+    "url_image": "./img/goty.png",
+    "desc": "Este impresionante juego hecho para los más exigentes, requiere de mucha habilidad.",
+    "cta": "Mostrar más...",
+    "link": "https://www.chiclestore.com/product/sekiro-shadows-die-twice-edicion-juego-del-ano-2/"
+  },
+  {
+    "title": "Juego del año",
+    "url_image": "./img/goty.png",
+    "desc": "Este impresionante juego hecho para los más exigentes, requiere de mucha habilidad.",
+    "cta": "Mostrar más...",
+    "link": "https://www.chiclestore.com/product/sekiro-shadows-die-twice-edicion-juego-del-ano-2/"
+  }
 ];
 
-(funtion () {
-
-let Card = {
-    init:funtion(){
-        let_self= this;
-        this.insertData(_self);
+(function() {
+  let Card = {
+    init: function() {
+      let _self = this;
+      this.insertData(_self);
     },
 
-    insertData:funtion(_self){
-dataCards.map(funtion (ClipboardItem,index){
-document.querySelector('.cards-list')insertAdjacentHTML('beforeend',_self.tplCardItem);
-
-});
-
+    insertData: function(_self) {
+      dataCards.map(function(item, index) {
+        document.querySelector('.cards-list').insertAdjacentHTML('beforeend', _self.tplCardItem(item, index));
+      });
     },
 
-    tplCardItem:funtion(item,index){
-        return(`div class='cards-item id='card-number-${index}'>
-            <img src='$item.url_image}'>
-            <div class='card-info'>
-            <p class='card-tittle'>${item.title}</p>
-            <p class='card-desc'>${item.desc}</p>
-<a class='card-cta' target='blank' href='${item.link}'>${item.cta}></a>
-            </div>)
-            </div>`)
+    tplCardItem: function(item, index) {
+      return (`<div class='cards-item' id='card-number-${index}'>
+        <img src='${item.url_image}'>
+        <div class='card-info'>
+          <p class='card-title'>${item.title}</p>
+          <p class='card-desc'>${item.desc}</p>
+          <a class='card-cta' target='_blank' href='${item.link}'>${item.cta}</a>
+        </div>
+      </div>`);
+    }
+  };
+  
+  Card.init();
+})();
 
-    },
-}
-CARD.init();
-        })();
